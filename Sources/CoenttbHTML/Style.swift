75d12db633ce4e73d1de923afe01becae2bfd4c0
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-06-25 14:20:12 +0200

    wip

---
+++ Sources/CoenttbHTML/Style.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  coenttb-html
+//
+//  Created by Coen ten Thije Boonkkamp on 25/06/2025.
+//
+
+import Foundation
+import HTML
+
+extension style {
+    public func callAsFunction(@StringBuilder _ content: () -> String) -> some HTML {
+        style(media: media, blocking: blocking, nonce: nonce, title: title) {
+            HTMLText(content())
+        }
+    }
+}
