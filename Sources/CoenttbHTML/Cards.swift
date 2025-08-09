c07c56d3c9699175eabf2eaee1a44615b421d995
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-08-09 10:26:05 +0200

    wip

---
+++ Sources/CoenttbHTML/Cards.swift
@@ -0,0 +1,71 @@
+//
+//  File.swift
+//  coenttb-html
+//
+//  Created by Coen ten Thije Boonkkamp on 09/08/2025.
+//
+
+import Foundation
+import HTML
+import Builders
+
+public struct Cards: HTML {
+    let columns: Cards.Columns
+    let cards: [any HTML]
+    
+    public init(
+        columns: Columns = .three,
+        @ArrayBuilder<any HTML> cards: () -> [any HTML]
+    ) {
+        self.cards = cards()
+        self.columns = columns
+    }
+
+    public var body: some HTML {
+        LazyVGrid(
+            columns: [.desktop: columns(cards.count)],
+            horizontalSpacing: .rem(1),
+            verticalSpacing: .rem(1)
+        ) {
+            HTMLForEach(cards.suffix(3).reversed()) { card in
+                AnyHTML(card)
+                    .maxWidth(.rem(24), media: .desktop)
+                    .margin(
+                        top: .rem(1),
+                        right: .zero,
+                        bottom: .rem(2),
+                        left: .zero
+                    )
+            }
+        }
+    }
+}
+
+extension Cards {
+    public struct Columns: Sendable {
+        public let closure: @Sendable (_ count: Int) -> [Int]
+        
+        public init(_ closure: @escaping @Sendable (_: Int) -> [Int]) {
+            self.closure = closure
+        }
+        
+        public func callAsFunction(_ count: Int) -> [Int] {
+            closure(count)
+        }
+        
+        public static func columns(_ maxColumns: Int) -> Self {
+            .init { count in
+                switch count {
+                case ...0: []
+                case 1: [1]
+                default: Array(repeating: 1, count: min(count, maxColumns))
+                }
+            }
+        }
+        
+        public static let two: Self = columns(2)
+        public static let three: Self = columns(3)
+        public static let four: Self = columns(4)
+        public static let five: Self = columns(5)
+    }
+}
